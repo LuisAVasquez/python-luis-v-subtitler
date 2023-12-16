27ddc497b444ff1f762071a6ecee411c840a98a4
LuisV <luisv@pop-os.localdomain>
2023-12-16 16:38:53 +0100

    Bump to 0.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="luis-v-subtitler",
-    version="0.0.3",
+    version="0.1.0",
     license="MIT",
     description="A Python package to use AI to subtitle any video in any language",
     long_description="{}\n{}".format(
