e5c2ad5da1dbc67c7adf775585a1213fa157febb
LuisV <luisv@pop-os.localdomain>
2023-12-28 20:22:22 +0100

    setting up logging leve

--- src/luis_v_subtitler/cli.py
+++ src/luis_v_subtitler/cli.py
@@ -14,9 +14,14 @@ Why does this file exist, and why not put this in __main__?
 
   Also see (1) from http://click.pocoo.org/5/setuptools/#setuptools-integration
 """
+import logging
+
 import click
 
 from .utils import basic_test
+
+# Configure logging settings
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 @click.command()
