3e1eca5f87b7f85a544085a793ce017bc6ea014b
LuisV <luisv@pop-os.localdomain>
2023-12-29 14:32:19 +0100

    Bump to 0.1.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="luis-v-subtitler",
-    version="0.1.2",
+    version="0.1.3",
     license="MIT",
     description="A Python package to use AI to subtitle any video in any language",
     long_description="{}\n{}".format(
--- src/luis_v_subtitler/__init__.py
+++ src/luis_v_subtitler/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from .subtitler import generate_subtitles_for_youtube
 
